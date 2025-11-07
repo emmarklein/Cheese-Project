c7a43d9d9b761100067117845becbb440c70cef0
emmarklein <emrklein@unc.edu>
2025-11-07 20:15:03 +0000

    cleaning script, removed shiny

--- cheese.R
+++ cheese.R
@@ -9,7 +9,7 @@ library(ggpubr)
 library(shiny)
 library(patchwork)
 
-# tidytugridExtra# tidytuesdayR package for cheese dataset
+# tidytuesdayR package for cheese dataset
 install.packages("tidytuesdayR")
 tuesdata <- tidytuesdayR::tt_load('2024-06-04')
 
@@ -371,57 +371,3 @@ plot6 <- ggplot(cheese_family_fat, aes(x = reorder(family, mean_fat), y = mean_f
   )
 
 ggsave("figures/plot6.png", plot6, width = 10, height = 5)
-
-########################### Let's make a Shiny app ##########################
-
-# i want to make an app so that a user can easily look at each plot!
-library(shiny)
-library(ggplot2)
-library(patchwork) # if you want to combine plots
-
-ui <- fluidPage(
-  titlePanel("Cheese Visualizations"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      selectInput(
-        inputId = "plot_choice",
-        label = "Choose a plot to display:",
-        choices = c(
-          "Top Cheesy Countries" = "plot1",
-          "Top Cheesy US States" = "plot2",
-          "Milk Types by Country" = "plot3",
-          "Cheese Types across Cheesy Countries" = "plot4",
-          "Fat Content by Cheese Type" = "plot5",
-          "Fat Content by Cheese Family" = "plot6"
-        )
-      )
-    ),
-    
-    mainPanel(
-      plotOutput("cheese_plot", height = "700px")
-    )
-  )
-)
-
-server <- function(input, output, session) {
-  
-  output$cheese_plot <- renderPlot({
-    if(input$plot_choice == "plot1") {
-      plot1
-    } else if(input$plot_choice == "plot2") {
-      plot2
-    } else if(input$plot_choice == "plot3") {
-      plot3
-    } else if(input$plot_choice == "plot4") {
-      plot4
-    } else if(input$plot_choice == "plot5") {
-      plot5
-    } else if(input$plot_choice == "plot6") {
-      plot6
-    }
-  })
-}
-
-shinyApp(ui, server)
-
